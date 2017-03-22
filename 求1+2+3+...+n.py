cf27cf60d65de8ebbc7859835e950dc9364d1a87
Mo <626407925@qq.com>
2017-03-23 04:23:19 +0800

    求1+2+3+...+n

---
+++ 求1+2+3+...+n.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Sum_Solution(self, n):
+        return n*(n+1)/2
+
+# 题目描述
+# 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+# 运行时间：20ms
+# 占用内存：3156k
