19bcfc2cdb4ddbd908c3528d93605afb18c2f6ce
Mo <626407925@qq.com>
2017-03-23 01:17:17 +0800

    修改 斐波那契数列

--- 斐波那契数列.py
+++ 斐波那契数列.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
 class Solution:
     def Fibonacci(self, n):
-        i, j = 0, 1
-        while n:
-            i, j = j, i+j
-            n = n - 1
-        return i
+        a = 0
+        b = 1
+        for i in range(n):
+            a,b = b,a+b
+        return a
         
 # 题目描述
 # 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
