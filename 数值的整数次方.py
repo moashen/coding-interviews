1dabd0832d66d1edd526f78163d81939b03edf97
Mo <626407925@qq.com>
2017-03-23 03:06:11 +0800

    数值的整数次方

---
+++ 数值的整数次方.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self, base, exponent):
+        return base**exponent
+
+# 题目描述
+# 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+# 运行时间：30ms
+# 占用内存：3156k
