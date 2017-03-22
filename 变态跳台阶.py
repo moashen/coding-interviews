368d977904329bc43e5312a5a35b61fff5ceba35
Mo <626407925@qq.com>
2017-03-23 01:25:11 +0800

    变态跳台阶

---
+++ 变态跳台阶.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def jumpFloorII(self, n):
+        return 2**(n-1)
+        
+# 题目描述
+# 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+# 运行时间：20ms
+# 占用内存：3156k
